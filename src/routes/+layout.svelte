95b199edf450c323e612b359b46f674cf10c949d
Tom Konink <konink1992@gmail.com>
2024-01-14 14:58:36 +0100

    start of a layout

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<header style="height: 56px; background-color: #E3801A;">
+    
+</header>
+
+<main>
+    <slot />
+</main>
+
+<style>
+	:global(html, body) {
+		height: 100%;
+		width: 100%;
+	}
+
+    :global(body){
+        margin: 0;
+    }
+</style>
