e6d299d54623bc8153ddddebda3ad38a33b043ee
Tom Konink <konink1992@gmail.com>
2024-01-19 23:37:44 +0100

    progress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,23 @@
-<header style="height: 56px; background-color: #E3801A;">
+<script lang="ts">
+function myFunction(myMessage:String) {
+    alert(myMessage);
+}
+</script>
+
+<!-- #FCFCF8-->
+<header style="height: 56px; background-color: #C66D19; color: #9E0003;">
+Takuma
     
 </header>
+<!-- #2A2925-->
+<header style="height: 56px; background-color: #E77C1A; color:#9E0003; ">
+Takuma
+
+<a href='http://www.google.com' on:click={() => myFunction('test')}>mylink</a>
+</header>
+
+
+<!-- #2A2925-->
 
 <main>
     <slot />
@@ -10,6 +27,7 @@
 	:global(html, body) {
 		height: 100%;
 		width: 100%;
+        background-color:#FCFCF8;
 	}
 
     :global(body){
